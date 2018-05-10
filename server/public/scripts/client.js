dbc8310dd08a82c5614553e7d8ff2ea6ae5222f0
katiemikul <katie.heding@gmail.com>
2018-05-10 14:47:44 -0500

    started home view and chose bootstrap for styling

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,1 +1,9 @@
 console.log('client.js has been loaded');
+
+let app = angular.module('PetApp', ['ngRoute']);
+
+app.controller('HomeController', ['$http', function($http) {
+    let self=this;
+    self.message = "Welcome to the Home Page";
+
+}])
